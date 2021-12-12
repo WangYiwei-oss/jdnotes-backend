ecc002f810736012133a739a49494ef7c91460fe
WangYiwei-oss <wangyiwei@seu.edu.cn>
2021-12-13 00:31:56 +0800

    完善注册controller

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/WangYiwei-oss/jdnotes-backend
 go 1.15
 
 require (
-	github.com/WangYiwei-oss/jdframe v0.0.0-20211209131323-e6f62d8e3de8
+	github.com/WangYiwei-oss/jdframe v0.0.0-20211212130630-adde7c77c55d
 	github.com/gin-gonic/gin v1.7.4
 )
--- src/controllers/LoginController.go
+++ src/controllers/LoginController.go
@@ -1,22 +1,36 @@
 package controllers
 
 import (
-	"fmt"
+	"encoding/json"
 	"github.com/WangYiwei-oss/jdframe/src/jdft"
 	"github.com/gin-gonic/gin"
 	"io/ioutil"
 )
 
 type LoginController struct {
+	Db *jdft.GormAdapter `inject:"-"`
 }
 
 func NewLoginController() *LoginController {
 	return &LoginController{}
 }
 
-func (l *LoginController)Login(ctx *gin.Context)int{
-	data,_ := ioutil.ReadAll(ctx.Request.Body)
-	fmt.Println(string(data))
+func (l *LoginController) Login(ctx *gin.Context) int {
+	type User struct {
+		Id       int    `gorm:"column:id" json:"id"`
+		UserName string `gorm:"column:username" json:"name"`
+		Password string `gorm:"column:password" json:"password"`
+	}
+	data, _ := ioutil.ReadAll(ctx.Request.Body)
+	var user1 User
+	err := json.Unmarshal(data, &user1)
+	if err != nil {
+		return -400
+	}
+	l.Db.Where("username = ? AND password = ?", user1.UserName, user1.Password).First(&user1)
+	if user1.Id == 0 {
+		return -100
+	}
 	return 1
 }
 
